2753e333bdbffab06c6e84520cbbd85601473dd3
Ujjwal <Ujjwal22154010@akgec.ac.in>
2023-10-02 16:40:22 +0530

    tricolor added

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ const textload = () => {
 
 textload();
 setInterval(textload,5000)
+const changecolorbutton = document.getElementById("changecolorbutton");
+const nochangecolorbutton = document.getElementById("nochangecolorbutton");
+const part1 =document.getElementById("part1");
+const part2 =document.getElementById("part2");
+const part3 =document.getElementById("part3");
+
+function colorchange(){
+    part1.style.backgroundColor ="orange";
+    part2.style.backgroundColor ="white";
+    part3.style.backgroundColor ="green";
+}
+function nocolorchange(){
+    part1.style.backgroundColor ="white";
+    part2.style.backgroundColor ="blue";
+    part3.style.backgroundColor ="white";
+}
+
+changecolorbutton.addEventListener("click",colorchange);
+changecolorbutton.removeEventListener("click",nocolorchange);
